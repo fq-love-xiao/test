62631187fa30499315bf2ebfcf895d4dd9d7745a
users <project@git.com>
2017-09-16 22:36:04 +0800

    水电费

---
+++ src/components/banner/banner.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="mint-swipe-item is-active" style="">
+		<a href="javascript:">
+			<div class="container">
+				<img src="">
+				<span class="title" v-text="">浪漫鲜花</span>
+			</div>
+		</a>
+	</div>
+</template>
+
+<script>
+	import Vue from 'vue'
+	import  Vuex from 'vuex'
+	Vue.use(Vuex)
+	const store = new Vuex.State({
+		state:{
+			goodsList:[]
+		},
+		mutations:{
+			
+		}
+	})
+</script>
+
+<style>
+	
+</style>
