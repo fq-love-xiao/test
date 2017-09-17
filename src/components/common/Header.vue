bc931713d51ffedb2ae798fdc3e3f8304ed44c62
wjcsun <wjc19941213>
2017-09-17 15:35:24 +0800

    he

---
+++ src/components/common/Header.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="header">
+		<p><</p>
+	</div>
+	
+</template>
+
+<script>
+</script>
+
+<style lang="less" rel="stylesheet/less">
+		*{
+			margin: 0;
+			padding: 0;
+		}
+		html{
+			font-size: 100px;
+		}
+		#header{
+			width:100%;
+			height:4.68rem;
+			background: red;
+			p{
+				font-size: 0.36rem;
+				width:100%;
+				background: blue;
+			}
+		}
+		
+</style>
